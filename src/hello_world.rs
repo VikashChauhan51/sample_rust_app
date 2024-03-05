ec822ee0f70b08a05954efba22a378545049b231
Vikash Chauhan <vikashchauhan51@gmail.com>
2024-03-05 19:12:38 +0530

    added sample function

--- src/hello_world.rs
+++ src/hello_world.rs
@@ -1,5 +1,10 @@
-
 /// print `Hello World` message on terminal/console window.
-pub fn msg(){
+pub fn msg() {
+    let result: i32 = sum(3, 5);
     println!("Hello, world!");
+    println!("the sum of two numbers x and y is:{result}")
+}
+
+fn sum(x: i32, y: i32) -> i32 {
+    x + y
 }
